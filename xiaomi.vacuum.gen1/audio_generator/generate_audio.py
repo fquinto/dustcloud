97600f84ebef0ce3fd04f4c89359cc8bc495fcb2
Reiner Herrmann <reiner@reiner-h.de>
2018-01-06 17:43:41 +0100

    Build encrypted sound package at the end of audio generation

--- xiaomi.vacuum.gen1/audio_generator/generate_audio.py
+++ xiaomi.vacuum.gen1/audio_generator/generate_audio.py
@@ -10,6 +10,31 @@ output_directory = "generated_" + language
 # text to speech engine (gtts, espeak or macos)
 engine = "gtts"
 
+def select_item(welcome_text, items):
+    print(welcome_text)
+    for i, item in enumerate(items):
+        print('{}. {}'.format(i+1, item))
+    try:
+        selected = input('Please select option by typing number (1-{}): '.format(len(items)))
+        result = items[int(selected)-1]
+        return result
+    except KeyboardInterrupt:
+        print('User requested to exit')
+        exit()
+    except ValueError:
+        print('Error! Please enter only one number')
+        exit()
+    except IndexError:
+        print('Error! Please enter one number between 1-{}'.format(len(items)))
+        exit()
+
+
+output_directory = "generated"
+available_audio = glob.glob('language/audio_*.csv')
+input_file = select_item('Available localized audio instructions:', available_audio)
+language = input_file.split('_')[-1].split('.')[0]
+sound_password = "r0ckrobo#23456"
+
 tts_engines = ['gtts']
 if sys.platform == 'darwin':
     tts_engines.append('macos')
@@ -56,3 +81,6 @@ for filename, text in filereader:
         # remove "-v Anna" if you want to use your system language, leave this as german default
         # format is recommendation from https://stackoverflow.com/a/9732070
         os.system("say -v Anna -o " + path + " --data-format=LEF32@22050 " + text)
+
+if os.system('cd %s && tar zc *.wav | ccrypt -e -K "%s" > %s.pkg' % (output_directory, sound_password, language)) == 0:
+    print("\nGenerated encrypted sound package at %s/%s.pkg" % (output_directory, language))
